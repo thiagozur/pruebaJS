364e1aa9d3d3b93420d95befaeb371ee5d4c5bd3
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-05-24 18:52:00 -0300

    agregado1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let numero = checkTempNumber(temperatura)
 
 let texto = (parseFloat(temperatura) < 0) ? "El arte es morirse de frío" :
     (0 <= parseFloat(temperatura) && parseFloat(temperatura) <= 16) ? "Está fresco para pasear en jaula" :
-    (16 < parseFloat(temperatura)) ? "Maldición, va a ser un día hermoso" : "Por favor, ingrese un número";
+    (16 < parseFloat(temperatura) && parseFloat(temperatura) <= 35) ? "Maldición, va a ser un día hermoso" : "Por favor, ingrese un número";
 
 var mensaje = document.getElementById("inputted")
 
