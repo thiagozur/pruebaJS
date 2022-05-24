9df2632a33129d108b36d547b450fb5cff774f7c
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-05-24 19:36:15 -0300

    agregado2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function checkTempNumber(temperatura) {
-    let check = isNaN(temperatura)
+    var check = isNaN(temperatura)
     return check
 }
 
 var temperatura = prompt("Ingrese una temperatura","")
 
-let numero = checkTempNumber(temperatura)
+var numero = checkTempNumber(temperatura)
 
-let texto = (parseFloat(temperatura) < 0) ? "El arte es morirse de frío" :
+var texto = (parseFloat(temperatura) < 0) ? "El arte es morirse de frío" :
     (0 <= parseFloat(temperatura) && parseFloat(temperatura) <= 16) ? "Está fresco para pasear en jaula" :
     (16 < parseFloat(temperatura) && parseFloat(temperatura) < 35) ? "Maldición, va a ser un día hermoso" :
     (parseFloat(temperatura) >= 35) ? "Muerte instantánea" : "Por favor, ingrese un número";
