e3297c927cb32511f378baa8649c8fc4984ce108
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-05-24 19:48:00 -0300

    agregado2

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function checkTempNumber(temperatura) {
-    var check = isNaN(temperatura)
-    return check
+    var check = isNaN(temperatura);
+    return check;
 }
 
-var temperatura = prompt("Ingrese una temperatura","")
+var temperatura = prompt("Ingrese una temperatura","");
 
-var numero = checkTempNumber(temperatura)
+var numero = checkTempNumber(temperatura);
 
 var texto = (parseFloat(temperatura) < 0) ? "El arte es morirse de frío" :
     (0 <= parseFloat(temperatura) && parseFloat(temperatura) <= 16) ? "Está fresco para pasear en jaula" :
     (16 < parseFloat(temperatura) && parseFloat(temperatura) < 35) ? "Maldición, va a ser un día hermoso" :
     (parseFloat(temperatura) >= 35) ? "Muerte instantánea" : "Por favor, ingrese un número";
 
-var mensaje = document.getElementById("inputted")
+var mensaje = document.getElementById("inputted");
 
-mensaje.innerText = texto
+mensaje.innerText = texto;
 
-var met = document.getElementById("temp")
+var met = document.getElementById("temp");
 
 if (numero) {
-    met.innerText = ""
+    met.innerText = "";
 } else {
-    met.innerText = temperatura
+    met.innerText = temperatura;
 }
